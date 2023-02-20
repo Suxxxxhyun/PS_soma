40be7ed14cea62870b97f250d9aafb065baa1e6f
hubris <darmaa333@gmail.com>
2023-02-20 16:11:56 +0900

    신규_백준_1654_랜선 자르기

---
+++ hu6r1s/python/boj/230220_1654_랜선 자르기.py
@@ -0,0 +1,17 @@
+'''
+다시 풀이 해볼 것.
+'''
+k, n = map(int, input().split())
+k_list = [int(input()) for _ in range(k)]
+start = 1
+end = max(k_list)
+while start <= end:
+    mid = (start + end) // 2
+    line = 0
+    for i in k_list:
+        line += i // mid
+    if line >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
