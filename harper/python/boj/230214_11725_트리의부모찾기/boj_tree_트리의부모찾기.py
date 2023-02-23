703e6e20fe3cf0783316ecc0f213988f7bfc2720
harper <jhjin2u@gmail.com>
2023-02-23 22:49:36 +0900

    복습_백준_11725_트리의부모찾기 -dfs -인접리스트로 인덱스가 부모, 값이 자식이니 자식 배열엔 인덱스 값을 넣어 부모를 찾을 수 있도록 함

--- harper/python/boj/230214_11725_트리의부모찾기/boj_tree_트리의부모찾기.py
+++ harper/python/boj/230214_11725_트리의부모찾기/boj_tree_트리의부모찾기.py
@@ -1,23 +1,22 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 n = int(input())
 tree = [[] for _ in range(n+1)]
 parent = [-1] * (n+1)
 
-for _ in range(n-1):
+for i in range(n-1):
   a, b = map(int, input().split())
   tree[a].append(b)
   tree[b].append(a)
 
-def dfs(node):
-  for new_node in tree[node]:
-    if parent[new_node] == -1:
-      parent[new_node] = node #직전 노드 기록 저장
-      dfs(new_node)
+def dfs(idx):
+
+  if idx == n:
+    return
+
+  for next in tree[idx]:
+    if parent[next] == -1:
+      parent[next] = idx
+      dfs(next)
 
 dfs(1)
-
 for i in range(2, n+1):
   print(parent[i])
-
