7df0d8ed142ecf7122139b00f954627dcd1a1dd3
harper <jhjin2u@gmail.com>
2023-02-23 11:44:57 +0900

    신규_프로그래머스_43162_네트워크 -dfs -인접행렬로 연결된 노드의 개수 반환

---
+++ harper/python/pg/230222_43162_네트워크/pg_dfs_네트워크.py
@@ -0,0 +1,18 @@
+def solution(n, computers):
+  def dfs(com):
+    visited[com] = True
+
+    for i in range(n):
+      if not visited[i] and computers[com][i] == 1:
+        dfs(i)
+
+  visited = [False] * (n+1)
+  answer = 0
+
+  for i in range(n):
+    for j in range(n):
+      if not visited[i] and computers[i][j] == 1:
+        dfs(i)
+        answer += 1
+
+  return answer
