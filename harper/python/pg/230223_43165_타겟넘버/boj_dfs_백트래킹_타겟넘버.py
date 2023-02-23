e0cddf7492ca10a3e1345cdee99de6a9446e7d25
harper <jhjin2u@gmail.com>
2023-02-23 15:53:57 +0900

    복습_백준_43165_타겟넘버 -dfs, 백트래킹

---
+++ harper/python/pg/230223_43165_타겟넘버/boj_dfs_백트래킹_타겟넘버.py
@@ -0,0 +1,21 @@
+sum = 0
+answer = 0
+
+def solution(numbers, target):
+  def dfs(idx):
+    global sum, answer
+    if idx == len(numbers):
+      if sum == target:
+        answer += 1
+      return
+
+    sum += numbers[idx]
+    dfs(idx+1)
+    sum -= numbers[idx]
+
+    sum -= numbers[idx]
+    dfs(idx+1)
+    sum += numbers[idx]
+
+  dfs(0)
+  return answer
