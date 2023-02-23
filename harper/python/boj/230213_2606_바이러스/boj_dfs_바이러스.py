f719280a881506b4cccf150d8d861fa88a51b5c4
harper <jhjin2u@gmail.com>
2023-02-23 11:05:32 +0900

    복습_백준_2606_바이러스 -dfs -인접리스트 만들어 연결된 노드의 수 세는 유형

--- harper/python/boj/230213_2606_바이러스/boj_dfs_바이러스.py
+++ harper/python/boj/230213_2606_바이러스/boj_dfs_바이러스.py
@@ -1,22 +1,25 @@
-computer = int(input())
-n =int(input())
-network = [[] * computer for _ in range(computer+1)]
-visited = [0] * (computer+1)
 
+
+def dfs(com):
+  global cnt
+  visited[com] = True
+
+  for i in network[com]:
+    if not visited[i]:
+      dfs(i)
+      cnt += 1
+
+computer = int(input())
+n = int(input())
+cnt = 0
+network = [[0] * (computer) for _ in range(computer+1)]
+visited = [False] * (computer+1)
 for _ in range(n):
   a, b = map(int, input().split())
   network[a].append(b)
   network[b].append(a)
-
-cnt = 0
-def dfs(x):
-  global cnt
-  visited[x] = 1
-  for computer in network[x]:
-    if visited[computer] == 0:
-      dfs(computer)
-      cnt += 1
-
+f
 dfs(1)
-print(cnt)
+print(cnt-1)
+
 
