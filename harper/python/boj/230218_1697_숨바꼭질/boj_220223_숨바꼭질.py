8919b689f35b271fdacb53a7c68c101715877da6
harper <jhjin2u@gmail.com>
2023-02-24 14:51:01 +0900

    복습_백준_1697_숨바꼭질 -bfs -최단거리유형

---
+++ harper/python/boj/230218_1697_숨바꼭질/boj_220223_숨바꼭질.py
@@ -0,0 +1,18 @@
+from collections import deque
+n, k = map(int, input().split())
+MAX = 100001
+second = [0] * MAX
+
+queue = deque()
+queue.append(n)
+
+while queue:
+  start = queue.popleft()
+  if start == k:
+    print(second[start])
+    break
+  for step in [start-1, start+1, start*2]:
+    if 0<=step<MAX and second[step] == 0:
+      second[step] = second[start] + 1
+      queue.append(step)
+
