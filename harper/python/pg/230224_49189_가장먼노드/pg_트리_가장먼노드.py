93f0960199450aa58b620ed4fe6bda3fb370f673
harper <jhjin2u@gmail.com>
2023-02-24 10:03:53 +0900

    신규_프로그래머스_49189_가장먼노드 -bfs, 트리 -가장먼노드의 최단거리와 같은 노드들 찾기 -특정거리의노드찾기와 비교

---
+++ harper/python/pg/230224_49189_가장먼노드/pg_트리_가장먼노드.py
@@ -0,0 +1,31 @@
+from collections import deque
+def solution(n, vertex):
+  answer = 0
+
+  tree = [[] * (n+1) for _ in range(n+1)]
+  distance = [-1] * (n+1)
+
+  for i in range(len(vertex)):
+    a, b = vertex[i]
+    tree[a].append(b)
+    tree[b].append(a)
+
+  queue = deque()
+  queue.append(1)
+  distance[1] = 1
+
+  while queue:
+    start = queue.popleft()
+    for next in tree[start]:
+      if distance[next] == -1: ####### 다음 노드가 방문하지 않았으면
+        distance[next] = distance[start] + 1
+        queue.append(next)
+
+
+  max_value = max(distance)
+  print(distance)
+
+  for i in range(1, n+1):
+    if max_value == distance[i]:
+      answer += 1
+  return answer
