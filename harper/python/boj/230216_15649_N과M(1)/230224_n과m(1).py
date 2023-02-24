d664ab0f44c7fc243d08ec10b051e0299dec6391
harper <jhjin2u@gmail.com>
2023-02-24 15:54:22 +0900

    복습_백준_15649_n과m -dfs, 백트래킹 -오름차순으로 뽑고 싶으면 index 이후로 선택 -중복을 제거하고 싶으면 리스트 안에 선택한 수가 있는지 체크 -" ".join(map(str, iterator))

---
+++ harper/python/boj/230216_15649_N과M(1)/230224_n과m(1).py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+list = []
+def dfs():
+  if len(list) == m:
+    print(" ".join(map(str, list))) #공백을 사이에 두고 출력
+    return
+
+  for i in range(1, n+1): #1부터 n까지
+    if i not in list: # 중복없이
+      list.append(i)
+      dfs()
+      list.pop()
+
+dfs()
